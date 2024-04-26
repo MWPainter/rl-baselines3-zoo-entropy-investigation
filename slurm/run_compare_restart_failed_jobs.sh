4e17f17778d2a0c973c83dbb35d7bd01c164e178
MWPainter <michaelpainter.1994@gmail.com>
2024-04-26 04:06:04 +0100

    Update restart jobs to restart failed job

--- slurm/run_compare_restart_failed_jobs.sh
+++ slurm/run_compare_restart_failed_jobs.sh
@@ -14,19 +14,3 @@ sbatch --job-name=${JOB_NAME} --output=${OUT_FILE} --export=EXPR_ID=${EXPR_ID},E
 
 
 
-
-
-EXPR_ID="compare_ppo_modes_v2"
-ENV_IDS_ARR=("HalfCheetah-v4" "Hopper-v4" "Humanoid-v4" "Walker2d-v4")
-#https://stackoverflow.com/questions/1527049/how-can-i-join-elements-of-a-bash-array-into-a-delimited-string
-ENV_IDS=$(IFS=';' ; echo "${ENV_IDS_ARR[*]}")
-
-OUT_DIR=slurm_out/${EXPR_ID}/mu
-mkdir -p $OUT_DIR
-
-# mujoco envs with ent_coef > 1e-5
-
-PPO_MODE="\"noent\"" 
-OUT_FILE=${OUT_DIR}/noent.out
-JOB_NAME=mu-${PPO_MODE}
-sbatch --job-name=${JOB_NAME} --output=${OUT_FILE} --export=EXPR_ID=${EXPR_ID},ENV_IDS=${ENV_IDS},PPO_MODE=${PPO_MODE} train_agent_many_env.slurm
